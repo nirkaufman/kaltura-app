afa8dd6686c4e0a13779993c3f2ef6fe92ed086a
nirkaufman <nirkaufman@gmail.com>
2017-07-26 14:42:29 +0300

    more code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,27 @@
 import {Component} from '@angular/core';
 
 @Component({
-  selector   : 'app-root',
-  template   : '<h1>Hello Angular</h1>',
+  selector: 'app-root',
+  template: `
+    <h1>{{ title }}</h1>
+    <input [type]="kind" #inputBox>
+    <button (click)="changeTitle(inputBox.value)">click me</button>
+  `,
 })
 export class AppComponent {
 
+  public title: string;
+  public kind = 'text';
+
+  constructor() {
+    this.title = 'Angular Application';
+  }
+
+  changeTitle(value) {
+    this.title = value;
+  }
+
+  getFormattedTitle() {
+    return this.title.toUpperCase();
+  }
 }
