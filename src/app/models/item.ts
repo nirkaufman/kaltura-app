a964e1b54b117286b678ad2c75f4870dcacb9e31
nirkaufman <nirkaufman@gmail.com>
2017-07-27 11:04:14 +0300

    models

---
+++ src/app/models/item.ts
@@ -0,0 +1,10 @@
+export class Item {
+
+  public title: string;
+  public completed: boolean;
+
+  constructor(title: string) {
+    this.title     = title;
+    this.completed = false;
+  }
+}
---
+++ src/app/todolist/todolist.service.ts
@@ -0,0 +1,18 @@
+import {Item} from '../models/item';
+
+export class TodolistService {
+
+  private _items: Item[];
+
+  constructor() {
+    this._items = [];
+  }
+
+  public get items(): Item[] {
+    return this._items;
+  }
+
+  public addItem(title: string): void {
+    this._items.push(new Item(title))
+  }
+}
