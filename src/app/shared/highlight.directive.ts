8a48f08cc659c6b1a3a4256eb3a8bfc192841850
nirkaufman <nirkaufman@gmail.com>
2017-07-31 09:38:47 +0300

    directive

--- src/app/shared/highlight.directive.ts
+++ src/app/shared/highlight.directive.ts
@@ -1,23 +1,36 @@
-import {Directive, HostListener, HostBinding, Input} from '@angular/core';
+import {Directive, Input, Renderer2, OnInit, ElementRef, HostListener, HostBinding} from '@angular/core';
 
 @Directive({
   selector: '[highlight]'
 })
-export class HighlightDirective {
+export class HighlightDirective  {
 
   @Input()
   public highlight: string;
-
   @HostBinding('style.background')
   public background;
+  private renderer: Renderer2;
+
+
+  // constructor(renderer: Renderer2, element:ElementRef) {
+  //   this.renderer = renderer;
+  //   this.element = element;
+  // }
+  //
+  // ngOnInit(): void {
+  //   this.renderer.listen(this.element.nativeElement, 'mouseenter', () => {
+  //     this.renderer.setStyle(this.element.nativeElement, 'background', 'yellow');
+  //   })
+  // }
+  private element: ElementRef;
 
   @HostListener('mouseenter')
-  public mark(){
+  public mark() {
     this.background = this.highlight;
   }
 
   @HostListener('mouseleave')
-  public clear(){
+  public clear() {
     this.background = '';
   }
 }
